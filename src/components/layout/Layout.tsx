4f7de8cde8d5ec829176473534318d152c74dc4e
Edgar Mejia <edgarinhm@hotmail.com>
2024-08-17 20:14:49 -0500

    add mui base dependencie

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,14 +1,17 @@
 import { Outlet } from 'react-router-dom';
+import { Button } from '@mui/base/Button';
 
 const Layout = () => {
   return (
     <main className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-xl m-2">{'Bookings'}</h1>
+        <Button className="bg-green-600 rounded-md py-1 px-4">
+          {'Create Booking'}
+        </Button>
       </div>
       <div>
         <Outlet />
-        {import.meta.env.VITE_SMTALENT_API_URL_BOOKINGS}
       </div>
     </main>
   );
