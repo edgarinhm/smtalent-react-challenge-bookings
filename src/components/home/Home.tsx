209ae64ede4546ae97b80dcd6e6d53dbc9712e67
Edgar Mejia <edgarinhm@hotmail.com>
2024-08-19 14:38:00 -0500

    add navbar styles add home image add home hotel reservation action

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,33 @@
+import { Button } from '@headlessui/react';
+
 const Home = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="w-full max-h-96">
+      <div>
+        <img
+          className="w-full h-full absolute object-cover left-0 right-0 m-0 mr-auto ml-auto z-10"
+          alt="Una persona vestida con ropa de verano está sentada en un sofá en una sala de estar espaciosa. Está mirando por la ventana y tiene un libro en una mano y una taza en la otra. Los grandes ventanales del salón ofrecen una vista marítima, con el mar al fondo y palmeras en primer plano."
+          src="https://q-xx.bstatic.com/xdata/images/xphoto/2880x868/363658458.jpeg?k=427a5cc2522eb3d80a76d232939725ec6cf76e03ef26ee846375709b3e9caf6f&amp;o="
+        ></img>
+      </div>
+      <div className="z-30 max-w-screen-lg min-h-80 m-0 ml-auto mr-auto relative flex px-1 py-12">
+        <div className="mt-auto mb-auto m-0">
+          <header className="text-white">
+            <h1 className="flex flex-col font-extrabold text-5xl">
+              <span>{'Siéntete como en casa'}</span>
+              <span>{'en tu próxima aventura'}</span>
+            </h1>
+            <p className="mt-1">
+              {'Disfruta de un alojamiento entero para ti'}
+            </p>
+            <Button className="mt-8 inline-flex items-center gap-2 rounded-md bg-blue-600 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-blue-700 data-[open]:bg-blue-700 data-[focus]:outline-1 data-[focus]:outline-white">
+              {'Reserva un hotel'}
+            </Button>
+          </header>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -17,7 +17,7 @@ const NavBar = () => {
   const navigate = useNavigate();
 
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-blue-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
@@ -27,7 +27,7 @@ const NavBar = () => {
                   <Link
                     key={item.name}
                     to={item.href}
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                    className="text-white hover:text-white hover:bg-white hover:bg-opacity-10 rounded-md px-3 py-2 text-sm font-medium"
                   >
                     {item.name}
                   </Link>
@@ -38,7 +38,7 @@ const NavBar = () => {
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <button
               type="button"
-              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+              className="relative rounded-full  p-1 text-white hover:text-white hover:bg-white hover:bg-opacity-10 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
               onClick={() => (auth ? logoutUser() : navigate('/login'))}
             >
               <span className="absolute -inset-1.5" />
