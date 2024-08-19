cc7d03cd15329759340b23abfb4e0c3537361926
Edgar Mejia <edgarinhm@hotmail.com>
2024-08-19 16:55:13 -0500

    fix active navbar style

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,20 +1,24 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { Disclosure } from '@headlessui/react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Disclosure, Menu, MenuButton } from '@headlessui/react';
+
 import {
   ArrowLeftEndOnRectangleIcon,
   ArrowLeftStartOnRectangleIcon,
 } from '@heroicons/react/24/outline';
+import { MdHome, MdOutlineBedroomChild } from 'react-icons/md';
+
 import useAuth from '../../common/hooks/useAuth';
 import { homeRoute, hotelRoute } from '../../routes';
 
 const navigation = [
-  { name: 'Home', href: homeRoute },
-  { name: 'Hotels', href: hotelRoute },
+  { name: 'Home', href: homeRoute, icon: MdHome },
+  { name: 'Hotels', href: hotelRoute, icon: MdOutlineBedroomChild },
 ];
 
 const NavBar = () => {
   const { auth, logoutUser } = useAuth();
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   return (
     <Disclosure as="nav" className="bg-blue-800">
@@ -22,17 +26,20 @@ const NavBar = () => {
         <div className="relative flex h-16 items-center justify-between">
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                {navigation.map((item) => (
-                  <Link
-                    key={item.name}
-                    to={item.href}
-                    className="text-white hover:text-white hover:bg-white hover:bg-opacity-10 rounded-md px-3 py-2 text-sm font-medium"
-                  >
-                    {item.name}
-                  </Link>
-                ))}
-              </div>
+              <Menu>
+                <div className="flex space-x-4">
+                  {navigation.map((item) => (
+                    <MenuButton
+                      key={item.name}
+                      id={item.name}
+                      className={`inline-flex items-center  gap-1 rounded-full py-1.5 px-3 text-sm/6 text-white shadow-inner shadow-white/10 focus:outline-none  data-[hover]:bg-white data-[hover]:bg-opacity-10 ${pathname === item.href ? 'outline outline-1 outline-white' : ''}`}
+                    >
+                      {<item.icon className="size-6" />}
+                      <Link to={item.href}>{item.name}</Link>
+                    </MenuButton>
+                  ))}
+                </div>
+              </Menu>
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
@@ -42,7 +49,7 @@ const NavBar = () => {
               onClick={() => (auth ? logoutUser() : navigate('/login'))}
             >
               <span className="absolute -inset-1.5" />
-              <span className="sr-only">View notifications</span>
+              <span className="sr-only">SignOut</span>
               {auth && (
                 <ArrowLeftStartOnRectangleIcon
                   className="h-6 w-6"
