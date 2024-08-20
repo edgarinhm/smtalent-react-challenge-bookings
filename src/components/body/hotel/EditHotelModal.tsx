fbdd367e32671e4c37b3be4e5f4f4f121c2aa0ee
Edgar Mejia <edgarinhm@hotmail.com>
2024-08-19 22:22:40 -0500

    use input from headless ui

--- src/components/body/hotel/EditHotelModal.tsx
+++ src/components/body/hotel/EditHotelModal.tsx
@@ -6,6 +6,7 @@ import { HotelModel } from '../../../common/models/hotel-model';
 import { FormEvent, useEffect, useState } from 'react';
 import Modal from '../../../common/components/Modal';
 import Spinner from '../../../common/components/Spinner';
+import { Input } from '@headlessui/react';
 
 interface EditHotelModalProps {
   isOpen: boolean;
@@ -73,7 +74,7 @@ const EditHotelModal = ({
             >
               {'Nombre'}
             </label>
-            <input
+            <Input
               type="text"
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               value={hotel.name}
