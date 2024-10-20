cd4fe0e5c50c33523dad7ee0f0f48b2f6f9f2a26
edgarinhm <edgarinhm@users.noreply.github.com>
2024-10-20 20:18:25 +0000

    deploy: c6d560835a3d77d46b652d6bb36ad5c46b4f8563

---
+++ environment-config.js
@@ -0,0 +1,6 @@
+window["environment-config"] = Object.freeze({
+    "ReactRouteId": "smtalent-react-challenge-bookings",
+    "Environment": "stage",
+    "SMTalentWebsiteDomain": "https://edgarinhm.github.io",
+    "APIHostName": "https://smtalent-booking-challenge-backend.onrender.com",
+  });
