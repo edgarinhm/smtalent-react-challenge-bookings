783dcc90e8a7dc35de2a0440b07e0b63dad66638
Edgar Mejia <edgarinhm@hotmail.com>
2024-10-20 14:34:30 -0500

    add vite base config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/smtalent-react-challenge-bookings/', //github pages
   test: {
     globals: true,
     environment: 'jsdom',
